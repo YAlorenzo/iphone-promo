4b9289475d60e13898fd5069551e259811e79547
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-05-26 21:10:16 +0200

    addThreeJsModel & Carousel

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,65 @@
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from "./IPhone";
+import * as THREE from "three";
+import Loader from "./Loader";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+    >
+      {/* Ambiet light --- для общего освещения сцены */}
+      <ambientLight intensity={0.3} />
+
+      {/* Перспективная камера моделирует, как человеческий глаз видит мир: объекты, находящиеся дальше, кажутся меньше. */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.currnet.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1 ? "small" : "large"}`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Loader />
+          }
+        >
+                  <IPhone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
+        </Suspense>
+      </group>
+    </View>
+  );
+};
+
+export default ModelView;
